71f0adae74d5e0bb11ab944534dce1afe1003dc5
Mcc <macharitocruzuk@gmail.com>
2023-01-07 19:48:00 +0000

    working version - prompt for password criteria and alert if the criteria is not met.

--- script.js
+++ script.js
@@ -88,62 +88,74 @@ let upperCasedCharacters = [
   "Z",
 ];
 
-// const charSet = [
-//   specialCharacters +
-//     numericCharacters +
-//     upperCasedCharacters +
-//     lowerCasedCharacters,
-// ];
-// charSet(specialCharacters);
-// charSet(numericCharacters);
-// charSet(upperCasedCharacters);
-// charSet(lowerCasedCharacters);
-
 // Function to prompt user for password options
 //CONCAT ARRAYS
 //const = ()
 
-const charSet = [];
-// let charSet = specialCharacters.concat(
-//   numericCharacters,
-//   upperCasedCharacters,
-//   lowerCasedCharacters
-// );
-const minCharLen = 10;
-const maxCharLen = 64;
+const charSet = specialCharacters.concat(
+  numericCharacters,
+  upperCasedCharacters,
+  lowerCasedCharacters
+);
+// specialCharacters = true;
+// numericCharacters = true;
+// upperCasedCharacters = true;
+// lowerCasedCharacters = true;
+
+let passLength = 0;
+let passLengthResponse = "";
+let specialCharRequired = "";
+let upperCaseRequired = "";
+let lowerCaseRequired = "";
+let numericRequired = 0;
+
+let password = "";
 
 function getPasswordOptions() {
-  charSet = charSet.concat(
-    specialCharacters,
-    numericCharacters,
-    upperCasedCharacters,
-    lowerCasedCharacters
-  );
+  let passLengthResponse = prompt("What lenght of password do you require?");
+  passLength = Number(passLengthResponse);
+  if (passLength > 9 && passLength < 65) {
+    passLengthResponse = charSet.length < 10 || charSet.length > 64;
+    specialCharRequired = confirm("Do you require Special Characters?");
+    upperCaseRequired = confirm("Do you want Uppercase Characters?");
+    lowerCaseRequired = confirm("Do you want Lowercase Characters?");
+    numericRequired = confirm("Do you want Numeric?");
+  } else {
+    alert("Please enter a number between 10 and 64.");
+  }
+
+  // console.log(passLength);
+  // if ([passLenght === true]) {
+  // alert ("Please write a number between 10 and 64." )
+  //   } else {
+  //     alert ('Please enter a number between 10 and 64.')
 }
+// }
+// console.log(getPasswordOptions);
 
 // if (upperCasedCharacters === false) {
 // } else {
-//   alert("Uppercase");
+//   alert("Must have Uppercase.");
 // }
 // if (lowerCasedCharacters === false) {
 // } else {
-//   alert("Lowercase");
+//   alert("Must have Lowercase.");
 // }
 // if (numericCharacters === false) {
 // } else {
-//   alert("Numeric");
+//   alert("Must have Numeric.");
 // }
 // if (specialCharacters === false) {
 // } else {
-//   alert("Special characters ($@%&*, etc)");
+//   alert("Must have  characters ($@%&*, etc).");
 // }
 // if (minCharLen === false) {
 // } else {
-//   alert("At least 10 characters but no more than 64");
+//   alert("password at least 10 characters but no more than 64.");
 // }
 // if (maxCharLen === false) {
 // } else {
-//   alert("At least 10 characters but no more than 64");
+//   alert("password at least 10 characters but no more than 64.");
 // }
 
 // Function for getting a random element from an array
@@ -151,7 +163,7 @@ function getPasswordOptions() {
 function getRandomCharSet() {
   return Math.floor(Math.random() * 64);
 }
-console.log(getRandomCharSet());
+// console.log(getRandomCharSet());
 
 // function charSet() {
 //   for (let i = 0; i < charSet.length; i++) {
@@ -168,7 +180,10 @@ console.log(getRandomCharSet());
 // Function to generate password with user input
 function generatePassword() {
   let practicePassword = "";
-  for (let i = 0; i < 64; i++) {
+
+  getPasswordOptions();
+
+  for (let i = 0; i < passLength; i++) {
     practicePassword += "j";
   }
 
@@ -184,7 +199,13 @@ function writePassword() {
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  //should call validation??
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function passGenerated(password) {
+  return alert(passGenerated);
+}
